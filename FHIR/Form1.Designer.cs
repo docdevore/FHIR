794e6a226d20f1a8b9827edbfa250ed17238ca69
Doc DeVore <docdevore@gmail.com>
2018-02-10 13:50:15 -0500

    search text box mod

--- FHIR/Form1.Designer.cs
+++ FHIR/Form1.Designer.cs
@@ -56,7 +56,7 @@
             // 
             this.textSearchName.Location = new System.Drawing.Point(52, 13);
             this.textSearchName.Name = "textSearchName";
-            this.textSearchName.Size = new System.Drawing.Size(227, 20);
+            this.textSearchName.Size = new System.Drawing.Size(350, 20);
             this.textSearchName.TabIndex = 2;
             // 
             // Form1
